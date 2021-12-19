7c439335a8509299949603b3b3cf0a3e368912d5
jogoo <joerironald@gmail.com>
2021-12-19 19:00:13 +0300

    10

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import about, home, contact
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 
 ]
